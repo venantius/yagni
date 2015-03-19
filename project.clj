6c08622ce7dc557683ccbabc36d6e801e5c790f8
David Jarvis <david@standardtreasury.com>
2015-03-19 15:03:44 -0700

    [deps] Remove tools.analyzer.jvm dependency
    t.a.j. causes some rather unpleasant dependency problems. There is
    probably a way around this but for the moment I'm not going to worry
    about it too much since Yagni doesn't actually use it anyways.

--- project.clj
+++ project.clj
@@ -4,6 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.analyzer.jvm "0.6.6"]
-                 [org.clojure/tools.namespace "0.2.10"]]
+                 [org.clojure/tools.analyzer.jvm "0.6.6"]]
   :eval-in-leiningen true)
