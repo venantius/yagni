fd4048a5e93ad1abd70d291df11499e57a1df6a3
David Jarvis <david@standardtreasury.com>
2015-03-23 22:30:38 -0700

    [tests] tests for `yagni.namespace.form-test`

--- test/yagni/namespace/dir_test.clj
+++
@@ -1,5 +0,0 @@
-(ns yagni.namespace.dir-test
-  (:require [clojure.test :refer :all]))
-
-(deftest nss-in-dirs-works
-  (is (= 0 1)))
--- test/yagni/namespace/form_test.clj
+++ test/yagni/namespace/form_test.clj
@@ -1,5 +1,32 @@
 (ns yagni.namespace.form-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [yagni.namespace.form :as form]
+            [yagni.sample-ns]
+            ))
+
+(deftest get-form-works
+  (is (= (form/get-form 'clojure.core/conj)
+         '(def conj
+            (fn conj ([coll x] (. clojure.lang.RT (conj coll x)))
+              ([coll x & xs]
+               (if xs (recur (conj coll x) (first xs) (next xs)) (conj coll x))))))))
+
+(deftest try-to-resolve-works
+  (is (= (form/try-to-resolve 'conj)
+         #'clojure.core/conj))
+  (is (= (form/try-to-resolve 'bananas)
+         nil)))
+
+(deftest maybe-inc-works
+  (with-redefs [form/fn-counter (atom {'clojure.core/conj 0
+                                       'clojure.core/bananas 0})] 
+    (form/maybe-inc 'conj)
+    (form/maybe-inc 'clojure.core/bananas)
+    (is (= @form/fn-counter {'clojure.core/conj 1
+                             'clojure.core/bananas 0}))))
 
 (deftest count-fns-works
-  (is (= 0 1)))
+  (with-redefs [form/fn-counter (atom {'yagni.sample-ns/y 0})] 
+    (form/count-fns ['yagni.sample-ns])
+    (is (= @form/fn-counter
+           {'yagni.sample-ns/y 1}))))
