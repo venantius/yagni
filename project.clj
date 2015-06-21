bed962e0b2e7a6a716eeffafb6b6aa1706bad912
David Jarvis <david@standardtreasury.com>
2015-06-21 16:16:44 -0700

    [yagni] Updating internal dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject venantius/yagni "0.1.0"
+(defproject venantius/yagni "0.1.1"
   :description "A Leiningen plugin for finding dead code."
   :url "https://github.com/venantius/yagni"
   :license {:name "Eclipse Public License"
--- src/leiningen/yagni.clj
+++ src/leiningen/yagni.clj
@@ -7,7 +7,7 @@
    walker."
   [project & args]
   (let [opts (select-keys project [:source-paths :yagni :main])
-        deps {:dependencies [['venantius/yagni "0.1.0-SNAPSHOT"]]}]
+        deps {:dependencies [['venantius/yagni "0.1.1"]]}]
     (eval-in-project
       (merge-profiles project [deps])
       `(yagni.core/run-yagni '~opts)
