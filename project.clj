cbbd44cba7ca174a540d0c6f8ca0568ccf0a1e59
Ursa Americanus Kermodei <venantius@gmail.com>
2015-07-06 16:45:20 -0700

    [deps] Removing dependency on tools.analyzer.jvm

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[im.chit/hara.class "2.1.8"]
                  [org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.analyzer.jvm "0.6.7"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/tools.namespace "0.2.11"]
                  [org.slf4j/slf4j-log4j12 "1.7.12"]]
